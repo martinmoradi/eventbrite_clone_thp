fb117fa871f2c28539966eb68075374e835713b7
Martin Moradi <moradi.martin@gmail.com>
2020-11-02 16:21:00 +0100

    Models & Tables

--- db/migrate/20201102144617_add_stripe_customer_id_to_attendances.rb
+++ db/migrate/20201102144617_add_stripe_customer_id_to_attendances.rb
@@ -1,5 +1,5 @@
 class AddStripeCustomerIdToAttendances < ActiveRecord::Migration[6.0]
   def change
-    add_column :attendance, :stripe_customer_id, :string
+    add_column :attendances, :stripe_customer_id, :string
   end
 end
